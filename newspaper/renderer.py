a8476698b4971c0baf81452cb8708c98a555ab8f
Hrvoje Milkovic <hrvoje@krakensystems.co>
2019-10-01 14:37:26 +0200

    reuse browser instance

--- newspaper/renderer.py
+++ newspaper/renderer.py
@@ -2,21 +2,24 @@ import asyncio
 from pyppeteer import launch
 import time
 
+browser = None
+
 async def render_js(url, headless=False, proxy=None, sleep=0.1):
     args = ['--no-sandbox']
     if proxy:
         args.append('--proxy-server=%s' % proxy)
-    browser = await launch(headless=headless, ignoreHTTPSErrors=True, dumpio=True, autoClose=False, args=args)
+    global browser
+    if not browser:
+        browser = await launch(headless=headless, ignoreHTTPSErrors=True, dumpio=True, args=args)
     page = await browser.newPage()
     await page.goto(url)
     if sleep:
         time.sleep(sleep)
     html = await page.content()
-    await browser.close()
-    page = None
-    browser = None
+    await page.close()
     return html
 
 def rendered_page(url, headless=True, proxy=None, sleep=0.1):
-    html = asyncio.run(render_js(url, headless=headless, proxy=proxy, sleep=sleep))
+    loop = asyncio.get_event_loop()
+    html = loop.run_until_complete(render_js(url, headless=headless, proxy=proxy, sleep=sleep))
     return html
--- test.py
+++ test.py
@@ -8,5 +8,5 @@ print(article.html)
 TorHandler().new_ip()
 
 article = ArticleParser('https://www.24sata.hr/news/kaos-u-hong-kongu-policajci-pravim-mecima-na-prosvjednika-651379test', MIN_WORD_COUNT=20)
-article.download(proxy='http://127.0.0.1:8118', js=True, headless=False, sleep=1)
+article.download(proxy='http://127.0.0.1:8118', js=True, headless=False, sleep=10)
 print(article.html)
