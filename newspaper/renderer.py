add34a83a650f64dba890095725f8838e72a3287
Hrvoje Milkovic <hrvoje@krakensystems.co>
2019-10-01 15:08:52 +0200

    better renderer

--- newspaper/renderer.py
+++ newspaper/renderer.py
@@ -1,25 +1,15 @@
-import asyncio
-from pyppeteer import launch
+from selenium import webdriver
 import time
 
-browser = None
-
-async def render_js(url, headless=False, proxy=None, sleep=0.1):
-    args = ['--no-sandbox']
+def rendered_page(url, headless=False, proxy=None, sleep=0.1):
+    chrome_options = webdriver.ChromeOptions()
+    if headless:
+        chrome_options.add_argument('--headless')
     if proxy:
-        args.append('--proxy-server=%s' % proxy)
-    global browser
-    if not browser:
-        browser = await launch(headless=headless, ignoreHTTPSErrors=True, dumpio=True, args=args)
-    page = await browser.newPage()
-    await page.goto(url)
-    if sleep:
-        time.sleep(sleep)
-    html = await page.content()
-    await page.close()
-    return html
-
-def rendered_page(url, headless=True, proxy=None, sleep=0.1):
-    loop = asyncio.get_event_loop()
-    html = loop.run_until_complete(render_js(url, headless=headless, proxy=proxy, sleep=sleep))
+        chrome_options.add_argument('--proxy-server={}'.format(proxy))
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get(url)
+    time.sleep(sleep)
+    html = driver.page_source
+    driver.close()
     return html
