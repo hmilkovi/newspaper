5a467503ba7dc2f463ee4ae88c259506d18a56f2
Hrvoje Milkovic <hrvoje@krakensystems.co>
2019-10-01 13:33:54 +0200

    fix event loop

--- newspaper/renderer.py
+++ newspaper/renderer.py
@@ -6,7 +6,7 @@ async def render_js(url, headless=False, proxy=None, sleep=0.1):
     args = []
     if proxy:
         args = ['--proxy-server=%s' % proxy]
-    browser = await launch(headless=headless, ignoreHTTPSErrors=True, dumpio=True, args=args)
+    browser = await launch(headless=headless, ignoreHTTPSErrors=True, dumpio=True, autoClose=False, args=args)
     page = await browser.newPage()
     await page.goto(url)
     if sleep:
@@ -16,4 +16,7 @@ async def render_js(url, headless=False, proxy=None, sleep=0.1):
     return html
 
 def rendered_page(url, headless=True, proxy=None, sleep=0.1):
-    return asyncio.get_event_loop().run_until_complete(render_js(url, headless=headless, proxy=proxy, sleep=sleep))
+    loop = asyncio.get_event_loop()
+    html = loop.run_until_complete(render_js(url, headless=headless, proxy=proxy, sleep=sleep))
+    loop.close()
+    return html
