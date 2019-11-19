85bf5a72ee610ba2a232a0afa9a81c6f6eed7df7
Hrvoje Milkovic <hrvoje@krakensystems.co>
2019-11-19 11:30:08 +0100

    minor fix

--- newspaper/renderer.py
+++ newspaper/renderer.py
@@ -10,11 +10,17 @@ def rendered_page(url, headless=False, proxy=None, sleep=0.1):
         chrome_options.add_argument('--headless')
     if proxy:
         chrome_options.add_argument('--proxy-server={}'.format(proxy))
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.set_window_size(1920, 1200)
     driver.get(url)
     time.sleep(sleep)
+    if 'One more step' in driver.page_source:
+        driver.quit()
+        return rendered_page(url, headless, None, sleep)
     html = driver.page_source
     time.sleep(0.5)
     driver.quit()
     return html
+
+if __name__ == "__main__":
+    rendered_page('https://telegraf.com.ua/ukraina/mestnyiy/5237623-rossiya-provodit-masshtabnyie-ucheniya-v-chernom-more.html', proxy='http://127.0.0.1:8119', sleep=10)
