61baf8878d20ff85f4b55212a038193670591752
Hrvoje Milkovic <hrvoje@krakensystems.co>
2019-11-21 11:54:16 +0100

    fix

--- newspaper/renderer.py
+++ newspaper/renderer.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 import time
 
-driver = None
 
 def rendered_page(url, headless=False, proxy=None, sleep=0.1):
-    global driver
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     userAgent = 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0'
@@ -16,22 +14,16 @@ def rendered_page(url, headless=False, proxy=None, sleep=0.1):
         chrome_options.add_argument('--headless')
     if proxy:
         chrome_options.add_argument('--proxy-server={}'.format(proxy))
-    try:
-        if driver == None:
-            driver = webdriver.Chrome(options=chrome_options)
-        driver.get(url)
-        time.sleep(sleep)
-        if 'One more step' in driver.page_source:
-            driver.quit()
-            return rendered_page(url, headless, None, sleep)
-        html = driver.page_source
-        time.sleep(0.5)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    time.sleep(sleep)
+    if 'One more step' in driver.page_source:
         driver.quit()
-        return html
-    except Exception as e:
-        print(e)
-        return rendered_page(url, headless, proxy, sleep)
-
+        return rendered_page(url, headless, None, sleep)
+    html = driver.page_source
+    time.sleep(0.5)
+    driver.quit()
+    return html
 
 if __name__ == "__main__":
     rendered_page('https://telegraf.com.ua/ukraina/mestnyiy/5237623-rossiya-provodit-masshtabnyie-ucheniya-v-chernom-more.html', proxy='http://127.0.0.1:8119', sleep=10)
